0ec3d05acd932e40db5ede4a1cd09f7fcfb2b054
lemon <1428828189@qq.com>
2024-02-23 02:58:22 +0800

    rvcc test1

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int Argc, char **Argv){
+    if(Argc != 2){
+        fprintf(stderr,"%s: invalid number of arguments\n", Argv[0]);
+        return 1;
+    }
+
+    printf("  .globl main\n");
+    printf("main:\n");
+    printf("  li a0, %d\n", atoi(Argv[1]));
+    printf("  ret\n");
+    
+    return 0;
+}
---
+++ tmp.s
@@ -0,0 +1,4 @@
+  .globl main
+main:
+  li a0, 42
+  ret
