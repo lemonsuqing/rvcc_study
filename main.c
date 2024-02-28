90b5107fe0cd57693a0b0c6db20b3b25fc0bcfb1
lemon <1428828189@qq.com>
2024-02-29 00:15:08 +0800

    [6] 支持一元运算的+ -

--- main.c
+++ main.c
@@ -171,6 +171,7 @@ typedef enum{
     ND_SUB, // -
     ND_MUL, // *
     ND_DIV, // /
+    ND_NEG, // 负号 - 不是减号
     ND_NUM, // 整型
 } NodeKind;
 
@@ -190,6 +191,13 @@ static Node *newNode(NodeKind Kind){
     return Nd;
 }
 
+// 新建一个单叉树，因为数字前的多个'-'号在构成二叉树的时候不需要左右分支
+static Node *newUnary(NodeKind Kind, Node *Expr){
+    Node *Nd = newNode(Kind);
+    Nd->LHS = Expr;
+    return Nd;
+}
+
 // 新建一个二叉树节点
 static Node *newBinary(NodeKind Kind, Node *LHS, Node *RHS){
     Node *Nd = newNode(Kind);
@@ -206,10 +214,13 @@ static Node *newNum(int Val){
 }
 
 // expr = mul ("+" mul | "-" mul)*
-// mul = primary ("*" primary | "/" primary)*
+// mul = primary ("*" unary | "/" unary)*
+// unary = ("+" | "-") unary | primary
+// 乘数由一元运算数构成，而一元运算数前面可能带有加号或者减号（多个）
 // primary = "(" expr ")" | num
 static Node *expr(Token **Rest, Token *Tok);
 static Node *mul(Token **Rest, Token *Tok);
+static Node *unary(Token **Rest, Token *Tok);
 static Node *primary(Token **Rest, Token *Tok);
 
 // 解析加减
@@ -237,22 +248,21 @@ static Node *expr(Token **Rest, Token *Tok) {
 }
 
 // 解析乘除
-// mul = primary ("*" primary | "/" primary)*
 static Node *mul(Token **Rest, Token *Tok) {
-  // primary
-  Node *Nd = primary(&Tok, Tok);
+  // unary
+  Node *Nd = unary(&Tok, Tok);
 
-  // ("*" primary | "/" primary)*
+  // ("*" unary | "/" unary)*
   while (true) {
-    // "*" primary
+    // "*" unary
     if (equal(Tok, "*")) {
-      Nd = newBinary(ND_MUL, Nd, primary(&Tok, Tok->Next));
+      Nd = newBinary(ND_MUL, Nd, unary(&Tok, Tok->Next));
       continue;
     }
 
-    // "/" primary
+    // "/" unary
     if (equal(Tok, "/")) {
-      Nd = newBinary(ND_DIV, Nd, primary(&Tok, Tok->Next));
+      Nd = newBinary(ND_DIV, Nd, unary(&Tok, Tok->Next));
       continue;
     }
 
@@ -261,8 +271,20 @@ static Node *mul(Token **Rest, Token *Tok) {
   }
 }
 
+// 解析一元运算数
+static Node *unary(Token **Rest, Token *Tok){
+    // '+' unary
+    if(equal(Tok, "+"))
+        return unary(Rest, Tok->Next);
+    // '-' unary
+    if(equal(Tok, "-"))
+        return newUnary(ND_NEG, unary(Rest, Tok->Next));
+    
+    // primary
+    return primary(Rest, Tok);
+}
+
 // 解析括号、数字
-// primary = "(" expr ")" | num
 static Node *primary(Token **Rest, Token *Tok) {
   // "(" expr ")"
   if (equal(Tok, "(")) {
@@ -308,10 +330,20 @@ static void pop(char *Reg) {
 
 // 生成表达式
 static void genExpr(Node *Nd) {
+  // 生成各个根节点
+  switch (Nd->Kind) {
   // 加载数字到a0
-  if (Nd->Kind == ND_NUM) {
+  case ND_NUM:
     printf("  li a0, %d\n", Nd->Val);
     return;
+  // 对寄存器取反
+  case ND_NEG:
+    genExpr(Nd->LHS);
+    // neg a0, a0是sub a0, x0, a0的别名, 即a0=0-a0
+    printf("  neg a0, a0\n");
+    return;
+  default:
+    break;
   }
 
   // 递归到最右节点
